c90902107f18406536f8ec45d76ebb67bb7a1461
Keerthana Sunder <keerubarbie@gmail.com>
2024-04-16 10:54:27 -0500

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.IntegerField()
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    class Meta:
+        db_table = 'users'
+    def __str__(self):
+        return self.name
 
 class Author(models.Model):
     author_id = models.AutoField(primary_key=True)
@@ -9,9 +19,7 @@ class Author(models.Model):
     organization = models.CharField(max_length=45)
 
     class Meta:
-        db_table = 'authors'  # The name of the table this model corresponds to
-        # If your database schema is named 'paper_pilot' you may need to prefix the table name
-        # with the schema name like 'paper_pilot.authors'. This depends on your DB setup.
+        db_table = 'authors'
 
     def __str__(self):
         return self.name
